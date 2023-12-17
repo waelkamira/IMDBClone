84f549b1171b7ffd0adae82998de031874837cf8
waelkamira <ramondm4@gmail.com>
2023-12-17 16:17:02 +0300

    fetch data from IMDB API

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
+import Results from "@/components/Results";
 import Image from "next/image";
-
-export default function Home() {
+const API_KEY = "56365df6a59a9d8c97697899dbba9313";
+// const API_KEY = process.env.API_KEY;
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    // `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`,
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "movie/popular"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await res.json();
+  const results = data.results;
+  console.log(results);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-red-600">Hello</h1>
-    </main>
+    <div>
+      <Results results={results} />
+    </div>
   );
 }
