13b78ea716e5f78aaf38cbeaae3f5cc31fe49d5d
waelkamira <ramondm4@gmail.com>
2023-12-21 01:44:26 +0300

    edit about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,22 +3,22 @@ function About() {
     <div className="max-w-6xl mx-auto space-y-4 p-4">
       <h1 className="text-2xl font-medium text-amber-600">About</h1>
       <p>
-        IMDb (an acronym for Internet Movie Database)[2] is an online database
-        of information related to films, television series, podcasts, home
-        videos, video games, and streaming content online – including cast,
-        production crew and personal biographies, plot summaries, trivia,
-        ratings, and fan and critical reviews. IMDb began as a fan-operated
-        movie database on the Usenet group "rec.arts.movies" in 1990, and moved
-        to the Web in 1993. Since 1998, it has been owned and operated by
-        IMDb.com, Inc., a subsidiary of Amazon.
+        IMDb an acronym for Internet Movie Database is an online database of
+        information related to films, television series, podcasts, home videos,
+        video games, and streaming content online including cast, production
+        crew and personal biographies, plot summaries, trivia, ratings, and fan
+        and critical reviews. IMDb began as a fan-operated movie database on the
+        Usenet group "rec.arts.movies" in 1990, and moved to the Web in 1993.
+        Since 1998, it has been owned and operated by IMDb.com, Inc., a
+        subsidiary of Amazon.
       </p>
       <br />
       <p>
         The site's message boards were disabled in February 2017. As of 2019,
         IMDb was the 52nd most visited website on the Internet, as ranked by
-        Alexa.[3] As of March 2022, the database contained some 10.1 million
-        titles (including television episodes), 11.5 million person records, and
-        83 million registered users.[4] Features The title and talent pages of
+        Alexa.3 As of March 2022, the database contained some 10.1 million
+        titles including television episodes, 11.5 million person records, and
+        83 million registered users 4 Features The title and talent pages of
         IMDb are accessible to all users, but only registered and logged-in
         users can submit new material and suggest edits to existing entries.
         Most of the site's data has been provided by these volunteers.
@@ -27,7 +27,7 @@ function About() {
         the addition and removal of images, and alterations to titles, cast and
         crew names, character names, and plot summaries are subject to an
         approval process; this usually takes between 24 and 72 hours. On October
-        2, 2007,[citation needed] character filmographies were added. Character
+        2, 2007,citation needed character filmographies were added. Character
         entries are created from character listings in the main filmography
         database, and as such do not need any additional verification by IMDb
         staff.
@@ -50,13 +50,13 @@ function About() {
       <br />
       <p>
         If a registered user or visitor is in the entertainment industry and has
-        an IMDb page, they can add photos through IMDbPRO.[5] User ratings of
-        films As one adjunct to data, the IMDb offers a rating scale that allows
-        users to rate titles on a scale of one to ten. IMDb indicates that
-        submitted ratings are filtered and weighted in various ways to produce a
-        weighted mean that is displayed for each title. It states that filters
-        are used to deter ballot stuffing; the methodology and details for how
-        its ratings are calculated are confidential and not accessible to the
+        an IMDb page, they can add photos through IMDbPRO. User ratings of films
+        As one adjunct to data, the IMDb offers a rating scale that allows users
+        to rate titles on a scale of one to ten. IMDb indicates that submitted
+        ratings are filtered and weighted in various ways to produce a weighted
+        mean that is displayed for each title. It states that filters are used
+        to deter ballot stuffing; the methodology and details for how its
+        ratings are calculated are confidential and not accessible to the
         public. In fact, it sometimes produces an extreme difference between the
         weighted average and the arithmetic mean. Rankings The IMDb Top 250 is a
         list of the top rated 250 films, based on ratings by the registered
