15455cf5f7699e48e36b581994b42a4a78e19f01
waelkamira <ramondm4@gmail.com>
2023-12-21 14:28:32 +0300

    fix errors

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -8,20 +8,20 @@ function About() {
         video games, and streaming content online including cast, production
         crew and personal biographies, plot summaries, trivia, ratings, and fan
         and critical reviews. IMDb began as a fan-operated movie database on the
-        Usenet group "rec.arts.movies" in 1990, and moved to the Web in 1993.
+        Usenet group rec.arts.movies in 1990, and moved to the Web in 1993.
         Since 1998, it has been owned and operated by IMDb.com, Inc., a
         subsidiary of Amazon.
       </p>
       <br />
       <p>
-        The site's message boards were disabled in February 2017. As of 2019,
+        The site is message boards were disabled in February 2017. As of 2019,
         IMDb was the 52nd most visited website on the Internet, as ranked by
         Alexa.3 As of March 2022, the database contained some 10.1 million
         titles including television episodes, 11.5 million person records, and
         83 million registered users 4 Features The title and talent pages of
         IMDb are accessible to all users, but only registered and logged-in
         users can submit new material and suggest edits to existing entries.
-        Most of the site's data has been provided by these volunteers.
+        Most of the site is data has been provided by these volunteers.
         Registered users with a proven track record are able to add and make
         corrections to cast lists, credits, and some other data points. However,
         the addition and removal of images, and alterations to titles, cast and
@@ -38,12 +38,12 @@ function About() {
         filmography. Registered users can choose their username, and most are
         pseudonymous. There is no single index of contributors, no index on each
         profile page of the items contributed, and—except for plot synopses and
-        biographies—no identification of contributors to each product's or
-        person's data pages. Users are also invited to rate titles on a scale of
+        biographies—no identification of contributors to each product is or
+        persons data pages. Users are also invited to rate titles on a scale of
         1 to 10, and the totals are converted into a weighted mean-rating, with
         filters in place to mitigate ballot-stuffing. User profile pages show a
-        user's registration date and, optionally, their personal ratings of
-        titles. Since 2015, "badges" can be added showing a count of
+        users registration date and, optionally, their personal ratings of
+        titles. Since 2015, badges can be added showing a count of
         contributions. These badges range from total contributions made to
         independent categories such as photos, trivia, and biographies.
       </p>
